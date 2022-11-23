03b8d601e3ffe32ca81121685324b6bd0ab7039a
Kevin Burkett <stlbucket@gmail.com>
2022-11-23 14:52:51 -0800

    links

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>fnb-nuxt-postgraphile</h1>
-    <h2>a boilerplate for graphql apps built with vue</h2>
+    <h2>a boilerplate for graphql apps built with <a target="_blank" href="https://www.vuejs.org">vue3</a>, <a target="_blank" href="https://www.nuxt.com">nuxt3</a> and <a target="_blank" href="https://www.graphile.org/postgraphile/">postgraphile</a>.</h2>
     <!-- default config is for bootstrapping local-only no password login -->
     <select 
       name="appUser" 
